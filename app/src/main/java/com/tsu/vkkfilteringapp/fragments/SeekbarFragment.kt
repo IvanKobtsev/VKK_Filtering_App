a079f14f838691931de0a78d730f5d8d264db6c1
nekitmix12 <nekitverh9@gmail.com>
2024-05-24 13:26:02 +0700

    add cycle retouching brush and  the coefficient of difference between the center and the end

--- app/src/main/java/com/tsu/vkkfilteringapp/fragments/SeekbarFragment.kt
+++ app/src/main/java/com/tsu/vkkfilteringapp/fragments/SeekbarFragment.kt
@@ -5,36 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.SeekBar
-import androidx.lifecycle.MutableLiveData
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
-import com.tsu.vkkfilteringapp.SeekbarData
 import com.tsu.vkkfilteringapp.TaskViewModel
 import com.tsu.vkkfilteringapp.databinding.FragmentSeekBarBinding
 
-class SeekbarFragment : Fragment() {
+class SeekBarFragment : Fragment() {
 
     private lateinit var binding: FragmentSeekBarBinding
     private lateinit var taskViewModel: TaskViewModel
-    private lateinit var currentSeekbarData: SeekbarData
-    private lateinit var currentLiveDataToEdit: MutableLiveData<Float>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         taskViewModel = ViewModelProvider(requireActivity())[TaskViewModel::class.java]
-
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-                currentSeekbarData.currentValue = progress
-                currentLiveDataToEdit.value = currentSeekbarData.getFloatValue()
-            }
-
-            override fun onStartTrackingTouch(seekBar: SeekBar) {}
-
-            override fun onStopTrackingTouch(seekBar: SeekBar) {}
-        })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -46,21 +30,15 @@ class SeekbarFragment : Fragment() {
 
     }
 
-    fun showBar(textView: Button, stringID: Int, liveDataToEdit: MutableLiveData<Float>, seekbarData: SeekbarData) {
+    fun showBar(textView: TextView, currentValue: Int, minValue: Int, maxValue: Int) {
 
-        taskViewModel.textViewToWrite.value = textView
-        taskViewModel.textViewStringID = stringID
-        binding.seekBar.progress = seekbarData.currentValue
-        binding.seekBar.max = seekbarData.getMax()
-        binding.minValue.text = seekbarData.getTextMin()
-        binding.maxValue.text = seekbarData.getTextMax()
+        //taskViewModel.textViewToWrite.value = textView
 
-        currentSeekbarData = seekbarData
-        currentLiveDataToEdit = liveDataToEdit
+
     }
 
     companion object {
         @JvmStatic
-        fun newInstance() = SeekbarFragment()
+        fun newInstance() = SeekBarFragment()
     }
 }
